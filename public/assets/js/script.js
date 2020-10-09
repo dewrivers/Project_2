0035bdfe839a89269b23bb499a5b9f38e87507b0
Brian GEARTY <geartybrian@gmail.com>
2020-10-09 11:04:04 -0400

    pushing for help on html routes to display database products for specific  category on page

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,29 +5,35 @@ $( document ).ready(function() {
 // Handling the click events for all the different Markets to display prodcuts from the DATABASE
 $('.category').on('click', function() {
     console.log('helloooo!!!', $(this).attr('name'))
+// brings us to specific market handlebars page.
+    window.location.href='/' + $(this).attr('name');
 
-    $.ajax({
-        url: '/search/' + $(this).attr('name')
-    }).then(function(results){
-        console.log('results', results)
-        $('.results').empty()
+//     $.ajax({
+//         url: '/search/' + $(this).attr('name')
+//     }).then(function(results){
+//         console.log('results', results)
+//         $('.results').empty()
+});       
 
-        for(var i = 0; i< results.length; i ++) {
-            console.log('we r looping!!!')
-            var div = $('<div>')
-            var title = $('<h1>')
-                title.text( results[i].Product_name)
-            var addToCart = $('<button>')
-                addToCart.text('Add to Cart')
-                addToCart.addClass("addToCart")
-                div.append(title, addToCart)
+//         for(var i = 0; i< results.length; i ++) {
+//             console.log('we r looping!!!')
+//             var div = $('<div>')
+//             var title = $('<h5 class=card-title>')
+//                 title.text( results[i].Product_name)
+//             var addToCart = $('<button>')
+//                 addToCart.addClass("addToCart")
+//                 addToCart.text('Add to Cart')
+//                 div.append(title, addToCart)
 
-            $('.results').append(div)
-        }
-    })
-})
+//             $('.results').append(div)
+//         }
+//     })
+// });
+
 
 // Handling the "ADD TO CART" click events to push specific item/product to the users cart.
 
-
+// $('.addToCart').on('click', function() {
+//     console.log('helloooo!!!', $(this).attr('name'))
+// })
 });
