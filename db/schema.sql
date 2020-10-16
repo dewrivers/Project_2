77305757be50520f442237ea1a9376f49caca5d6
Brian GEARTY <geartybrian@gmail.com>
2020-10-16 15:59:57 -0400

    adding heroku url to the heroku icon in footer

--- db/schema.sql
+++ db/schema.sql
@@ -2,46 +2,28 @@
 DROP DATABASE IF EXISTS marketplace_db;
 CREATE DATABASE marketplace_db;
 USE marketplace_db;
+
 -- When starting an order a customer will be prompted to give their first name, last name, and phone number to start their order in which their Id will automatically be generated.
-<<<<<<< HEAD
-Create table Customer (
-=======
 Create table Customer(
->>>>>>> 767c659068ebb7aafd8c30b6d3b7910197e047f2
 Id INT AUTO_INCREMENT NOT NULL,
 Whole_name varchar (30) NOT NULL,
 Email varchar(30) NOT NULL,
 Phone_number INT NOT NULL,
-PASSWORD varchar(20) NOT NULL,
 Primary Key(id)
 );
-<<<<<<< HEAD
-Alter table customer
-=======
-
-Alter table Customer
->>>>>>> 767c659068ebb7aafd8c30b6d3b7910197e047f2
-Add Foreign Key (id) references cart(id);
 
 
 -- This will be the specific items that the customer is purchasing referenced by the Category(market) that they ordered from
 Create table Cart (
 Id INT AUTO_INCREMENT NOT NULL,
-<<<<<<< HEAD
-Customerid INT NOT NULL,
-=======
 Customer_id INT NOT NULL,
->>>>>>> 767c659068ebb7aafd8c30b6d3b7910197e047f2
-Name varchar(30) NOT NULL,
+Food_id INT NOT NULL,
+Product_name varchar(30) NOT NULL,
 Cost DEC(10, 4) NOT NULL,
 Primary Key(id)
 );
-Alter table Cart
-<<<<<<< HEAD
-Add Foreign Key (Customerid) references Category(id);
-=======
-Add Foreign Key (Customer_id) references Food(id);
->>>>>>> 767c659068ebb7aafd8c30b6d3b7910197e047f2
+Add Foreign Key (CustomerId) references Customer(id);
+Add Foreign Key (FoodId) references Food(id);
 
 
 Create table Food (
